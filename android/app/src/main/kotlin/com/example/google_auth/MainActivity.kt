2f4d1993e9a869e9bea9f5f82096c2bf6df41ea1
Saeed Gamal <s.gamal.exab.sa>
2025-01-10 15:14:09 +0200

    initial

---
+++ android/app/src/main/kotlin/com/example/google_auth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.google.auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/login_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginScreen extends StatelessWidget {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _signInWithGoogle() async {
+    try {
+      // Trigger Google Sign-In flow
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return;
+
+      // Obtain auth details from the request
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      // Create a new credential
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+
+      // Sign in to Firebase with the credential
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      final User? user = userCredential.user;
+
+      if (user != null) {
+        print("Signed in: ${user.displayName}");
+        // Navigate to the home screen or perform other actions
+      }
+    } catch (e) {
+      print("Error signing in with Google: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Sign-In'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _signInWithGoogle,
+          child: Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
